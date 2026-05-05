5b795f4a2a57a352b827daeda24a0e8bfaa01c24
财哥 <aes6@qq.com>
2026-05-05 22:03:44 +0800

    test

---
+++ cppcode/test02.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10;
+    int b=20;
+    cout<<"a="<<a<<" b="<<b<<endl;
+    swap(a,b);
+    cout<<"a="<<a<<" b="<<b<<endl;
+    return 0;
+}
