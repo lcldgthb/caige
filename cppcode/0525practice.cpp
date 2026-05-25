bd38c239b5d2660e787007ae4e57ae96def597d4
财哥 <aes6@qq.com>
2026-05-25 23:33:44 +0800

    每日一题

---
+++ cppcode/0525practice.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+
+    return 0;
+}
+/*
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        string s=to_string(x);
+        int l=0,r=s.size()-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r])
+            return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
+*/
