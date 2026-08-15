8a4b6969597e55ba1df8a331732d88c0cd7ce3db
财哥 <aes6@qq.com>
2026-08-15 23:10:52 +0800

    整理博客

--- cppcode/daily_learning/封装map和set/封装map和set/mymap.h
+++ cppcode/daily_learning/封装map和set/封装map和set/mymap.h
@@ -41,6 +41,22 @@ namespace caige
 		{
 			return _map.Insert(kv);
 		}
+		/*iterator& operator[](const V& v,const K& k)
+		{
+			return Insert({ v,k }).first;
+		}*/
+		//复习一下运算符重载，错误理解
+		//单参数；返回值就是该参数
+		//双参数：第二个参数做返回值，第一个参数做参数
+		//正确理解
+		//一元运算符：成员函数 0 个显式参数（this 就是那唯一的操作数）
+		//二元运算符：成员函数 1 个显式参数（this 是左操作数，显式参数是右操作数）
+		V& operator[](const K& key)
+		{
+			pair<iterator, bool> ret = insert(make_pair(key, V()));
+			return ret.first->second;
+		}
+
 		
 	private:
 		RBTree<K, pair<const K, V>, keyofmap > _map;
