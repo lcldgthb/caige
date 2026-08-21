2c9a804441227573132084c0f2d00376f521679a
财哥 <aes6@qq.com>
2026-08-21 23:17:53 +0800

    复习

--- cppcode/daily_learning/C++11/C++11/tesst.cpp
+++ cppcode/daily_learning/C++11/C++11/tesst.cpp
@@ -1,3 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
+#include<vector>
+#include<list>
+#include<map>
 using namespace std;
+int main()
+{
+	vector<int> a = { 1,3,4,6,6,5 };
+	map<int, int> b = { {1,1},{2,2} };
+	int c = { 2 };
+	/*int x , y ;
+	int& a = move(x);
+	int&& b = 10;*/
+	return  0;
+
+}
